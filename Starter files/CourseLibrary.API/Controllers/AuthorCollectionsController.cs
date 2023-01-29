11de59313740cf98b8a68dbb6e7f8f7b4324e7d1
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-01-29 21:45:12 +0100

    add GetAuthorCollection() and ArrayModelBinder.cs

--- Starter files/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ Starter files/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,23 @@ public class AuthorCollectionsController : ControllerBase
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
+    [HttpGet("({authorIds})", Name = "GetAuthorCollection")]
+    public async Task<ActionResult<IEnumerable<AuthorForCreationDto>>> GetAuthorCollection(
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))]
+        [FromRoute] IEnumerable<Guid> authorIds)
+    {
+        IEnumerable<Author> authorEntities = await _courseLibraryRepository.GetAuthorsAsync(authorIds);
+
+        var areAllAuthorsFound = authorEntities.Count() == authorIds.Count();
+        if (!areAllAuthorsFound)
+        {
+            return NotFound();
+        }
+
+        IEnumerable<AuthorDto> authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+        return Ok(authorsToReturn);
+    }
+
     [HttpPost]
     public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(
         IEnumerable<AuthorForCreationDto> authorCollection)
@@ -33,7 +51,13 @@ public class AuthorCollectionsController : ControllerBase
         }
         await _courseLibraryRepository.SaveAsync();
 
-        //return CreatedAtRoute();
-        return Ok();
+
+        IEnumerable<AuthorDto> authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+        var authorIdsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+
+        return CreatedAtRoute(
+            routeName: "GetAuthorCollection",
+            routeValues: new { authorIds = authorIdsAsString },
+            value: authorCollectionToReturn);
     }
 }
