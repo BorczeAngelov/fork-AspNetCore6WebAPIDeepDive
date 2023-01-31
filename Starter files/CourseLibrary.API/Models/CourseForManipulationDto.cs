1a418b6662eaf523de044c3bdabf846ee1c1a21b
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-01-31 21:53:20 +0100

    add custom validationAttribute

--- Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public abstract class CourseForManipulationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public abstract class CourseForManipulationDto
     {
         [Required(ErrorMessage = "You should fill out a title.")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
@@ -11,16 +13,18 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public virtual string Description { get; set; } = string.Empty;
 
-        //  (Cross-property validation) Class-level input validation with IValidatableObject.
-        //  Validate a business rule for example
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult(
-                errorMessage: "The provided description should be different from the title.",
-                memberNames: new[] { "Course" });
-            }
-        }
+
+        //Replaced by CourseTitleMustBeDifferentFromDescriptionAttribute
+        ////  (Cross-property validation) Class-level input validation with IValidatableObject.
+        ////  Validate a business rule for example
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //        errorMessage: "The provided description should be different from the title.",
+        //        memberNames: new[] { "Course" });
+        //    }
+        //}
     }
 }
