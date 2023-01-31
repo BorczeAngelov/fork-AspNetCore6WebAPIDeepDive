fe4dd0aa17e2b6e3cf394410c8ad6d774caa9b55
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-01-31 21:47:54 +0100

    imp IValidatableObject

--- Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -2,7 +2,7 @@
 
 namespace CourseLibrary.API.Models
 {
-    public abstract class CourseForManipulationDto
+    public abstract class CourseForManipulationDto : IValidatableObject
     {
         [Required(ErrorMessage = "You should fill out a title.")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
@@ -10,5 +10,17 @@ namespace CourseLibrary.API.Models
 
         [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public virtual string Description { get; set; } = string.Empty;
+
+        //  (Cross-property validation) Class-level input validation with IValidatableObject.
+        //  Validate a business rule for example
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                errorMessage: "The provided description should be different from the title.",
+                memberNames: new[] { "Course" });
+            }
+        }
     }
 }
